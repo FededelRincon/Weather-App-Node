74944270ad72b837c41b41c29191e5d346f1163d
“fededelrincon” <delrincon@gmail.com>
2021-01-15 23:12:37 -0300

    031- Making HTTP Requests

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-console.log('Starting')
+// asi fue como hice la peticion x 1era vez
+// http://api.weatherstack.com/current?access_key=856eb07c434d80ac3cf4bfac9a5da8be&query=37.8267,-122.4233
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const request = require('postman-request')
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
+const url = 'http://api.weatherstack.com/current?access_key=856eb07c434d80ac3cf4bfac9a5da8be&query=37.8267,-122.4233'
 
-console.log('Stopping')
+request({ url:url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
