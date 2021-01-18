9566c250a9d3641af0c48744af50d5c57c33fc15
“fededelrincon” <delrincon@gmail.com>
2021-01-18 12:46:39 -0300

    033- Geolocalization API HTTP Request

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=856eb07c434d80ac3cf4bfac9a5da8be&query=37.8267,-122.4233&units=m'
 
-request({ url:url, json: true }, (error, response) => {
+//weather api
+const weatherURL = 'http://api.weatherstack.com/current?access_key=856eb07c434d80ac3cf4bfac9a5da8be&query=37.8267,-122.4233&units=m'
+
+request({ url:weatherURL, json: true }, (error, response) => {
     //console.log(response.body.current)
 
     const weater_descriptions = response.body.current.weather_descriptions[0]
@@ -13,4 +15,15 @@ request({ url:url, json: true }, (error, response) => {
     const feelsTemperature = response.body.current.feelslike
     
     console.log(`${weater_descriptions}. It is currently ${temperature} degress out there. It feels like ${feelsTemperature} degress out`)
+})
+
+
+//Geo api
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGFxdWV0aW5obyIsImEiOiJja2sycDc4YzAxM2kyMm9tZm8ybWg3emdrIn0.6XhGimBpY4H5p_zjExagpg&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+
+    console.log(latitude, longitude)
 })
